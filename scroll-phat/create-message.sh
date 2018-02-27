d1cfbe6aaede576017ce80d45e20ed774efe1b3b
BradleyA <allen.bradley@ymail.com>
2018-02-26 19:39:46 -0600

    worked the help and completed testing, 2 programs work

--- scroll-phat/create-message.sh
+++ scroll-phat/create-message.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	create-message.sh	2.5.35	2018-02-26_19:38:46_CST uadmin three-rpi3b.cptx86.com 2.4-1-gd14768e 
+# 	   worked the help and completed testing, 2 programs work 
 # 	create-message.sh	2.4.33	2018-02-26_17:06:02_CST uadmin three-rpi3b.cptx86.com 2.3 
 # 	   rewrote uptime todisplay-board.py 
 # 	display-board-1.sh	2.3.32	2018-02-26_15:14:25_CST uadmin three-rpi3b.cptx86.com 2.2-9-gfd85103 
@@ -20,16 +22,25 @@
 #	set -v
 ###
 display_help() {
-echo -e "\n${0} - display display container status"
-echo    "   UNDER DEVELOPMENT  . . . .  ."
-echo -e "\nUSAGE\n   ${0}"
+echo -e "\n${0} - stores information about containers and images"
+echo -e "\n   >>> UNDER DEVELOPMENT  . . . .  ."
+echo -e "\nUSAGE\n   ${0} [<cluster>] [<administrator>] [<data_directory>] [<sshport>"
 echo    "   ${0} [--help | -help | help | -h | h | -? | ?] [--version | -v]"
-echo -e "\nDESCRIPTION\nThis script "
+echo -e "\nDESCRIPTION\nThis script stores Docker information about containers and images in a file"
+echo    "on each system in a cluster.  These files are copied to a host and totaled"
+echo    "in a file, /usr/local/data/cluster-1/MESSAGE.  The MESSAGE file includes the"
+echo    "total number of conntainers, running containers, paused containers, stopped"
+echo    "containers, and number of images.  The MESSAGE file is used to display the"
+echo    "information on a Raspberry Pi Scroll-pHAT display."
+echo -e "\n   >>>   NEED to finish this... run script in a container every two minutes on a Raspberry Pi that includes Scroll-pHAT display."
 echo -e "\nOPTIONS"
-echo    "   arg1	XXXXXX"
-echo -e "\nDOCUMENTATION\n   https://github.com/BradleyA/display-board-2-project"
-echo -e "\nEXAMPLES\n   xxxxxxxx "
-echo -e "   xxxxxXXXXxxxxXXXX, x\n\tsudo ${0}\n"
+echo    "   CLUSTER   name of cluster directory, dafault cluster-1/"
+echo    "   ADMUSER   site SRE administrator, default is user running script"
+echo    "   DATA_DIR  path to cluster directory, dafault /usr/local/data/"
+echo    "   SSHPORT   SSH server port, default port 22"
+echo -e "\nDOCUMENTATION\n   https://github.com/BradleyA/pi-display-board"
+echo -e "\nEXAMPLES"
+echo -e "   Store information for a different cluster\n\t${0} cluster-2\n"
 }
 if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] || [ "$1" == "?" ] ; then
         display_help
@@ -41,7 +52,7 @@ if [ "$1" == "--version" ] || [ "$1" == "-v" ] || [ "$1" == "version" ] ; then
 fi
 ### 
 CLUSTER=${1:-cluster-1/}
-USER=${2:-${USER}}
+ADMUSER=${2:-${USER}}
 DATA_DIR=${3:-/usr/local/data/}
 SSHPORT=${4:-22}
 CONTAINERS=0
@@ -83,8 +94,8 @@ for NODE in ${NODE_LIST} ; do
 #	Check if ${NODE} is available on port ${SSHPORT}
 		if $(nc -z ${NODE##*/} ${SSHPORT} >/dev/null) ; then
 			TEMP="docker system info | head -5 > ${NODE}"
-			ssh -q -t -i ~/.ssh/id_rsa -p ${SSHPORT} ${USER}@${NODE##*/} ${TEMP}
-			scp -q    -i ~/.ssh/id_rsa -P ${SSHPORT} ${USER}@${NODE##*/}:${NODE} ${DATA_DIR}${CLUSTER}
+			ssh -q -t -i ~/.ssh/id_rsa -p ${SSHPORT} ${ADMUSER}@${NODE##*/} ${TEMP}
+			scp -q    -i ~/.ssh/id_rsa -P ${SSHPORT} ${ADMUSER}@${NODE##*/}:${NODE} ${DATA_DIR}${CLUSTER}
 		else
 			echo -e "${NORMAL}${0} ${LINENO} [${BOLD}WARN${NORMAL}]:        ${NODE##*/} not responding on port ${SSHPORT}.\n"   1>&2
 		fi
@@ -97,11 +108,10 @@ for NODE in ${NODE_LIST} ; do
 	STOPPED=`grep -i STOPPED ${NODE} | awk -v v=$STOPPED '{print $2 + v}'`
 	IMAGES=`grep -i IMAGES ${NODE} | awk -v v=$IMAGES '{print $2 + v}'`
 done
-#	echo    "${0} ${LINENO} -->${LOCALHOST}<--->${NODE##*/}<--->${SSHPORT}<--->${USER}<--"
-#	echo    "${0} ${LINENO} -->${CONTAINERS}<--->${RUNNING}<--->${PAUSED}<---->${STOPPED}<---->${IMAGES}<----"
-#	echo    "${0} ${LINENO} -->${LOCALHOST}<--n-->${NODE}<--dd-->${DATA_DIR}<--c-->${CLUSTER}<--P-->${SSHPORT}<--U-->${USER}<--"
-#	echo ${MESSAGE} > ${DATA_DIR}${CLUSTER}/MESSAGE
 MESSAGE=" CONTAINERS ${CONTAINERS}  RUNNING ${RUNNING}  PAUSED ${PAUSED}  STOPPED ${STOPPED}  IMAGES ${IMAGES} "
 echo ${MESSAGE} > ${DATA_DIR}${CLUSTER}/MESSAGE
-./scroll-text-rotated.py "${MESSAGE}"
+#	echo    "${0} ${LINENO} -->${LOCALHOST}<--->${NODE##*/}<--->${SSHPORT}<--->${ADMUSER}<--"
+#	echo    "${0} ${LINENO} -->${CONTAINERS}<--->${RUNNING}<--->${PAUSED}<---->${STOPPED}<---->${IMAGES}<----"
+#	echo    "${0} ${LINENO} -->${LOCALHOST}<--n-->${NODE}<--dd-->${DATA_DIR}<--c-->${CLUSTER}<--P-->${SSHPORT}<--U-->${ADMUSER}<--"
+#	echo ${MESSAGE} > ${DATA_DIR}${CLUSTER}/MESSAGE
 ###
