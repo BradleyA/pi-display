1e961cdb262a47065947ede711aeb305f56b6577
BradleyA <allen.bradley@ymail.com>
2018-03-14 22:00:08 -0500

    added more prices, first draft of display-led.py

--- blinkt/blinkt.mine/warn_colors_3.py
+++ blinkt/blinkt.mine/warn_colors_3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 	warn_colors_2.py  3.08.85  2018-03-14_21:59:15_CDT  https://github.com/BradleyA/pi-display  uadmin  two-rpi3b.cptx86.com 3.07-2-g5f6290c  
+# 	blinkt.mine/warn_colors_3.py  3.08.85  2018-03-14_21:59:15_CDT  https://github.com/BradleyA/pi-display  uadmin  two-rpi3b.cptx86.com 3.07-2-g5f6290c  
 # 	   added more prices, first draft of display-led.py 
 # 	warn_colors_2.py  2.7.39  2018-02-27_18:52:21_CST  https://github.com/BradleyA/pi-display-board  uadmin  two-rpi3b.cptx86.com 2.6-1-ge4f399b  
 # 	   moved this test cases to github 
@@ -15,6 +15,7 @@
 #	Other containers will update a volume that this container mounts
 #		and reads (LED_number, Level)
 #
+#   check this out https://psutil.readthedocs.io/en/latest/
 
 from blinkt import set_clear_on_exit, set_pixel, show, clear
 import time
@@ -22,24 +23,36 @@ import time
 set_clear_on_exit()
 LED_number = 0
 
+###
+if status == '1':
 #	GREEN : no known incidents
-set_pixel(LED_number, 0, 255, 0, 0.5)
-show()
-time.sleep(1) # 1 = 1 second
+    set_pixel(LED_number, 0, 255, 0, 1.0)
+    show()
+    time.sleep(1) # 1 = 1 second
+elif status == '2':
+    # Do the other thing
+if status == '3':
+    # Fall-through by not using elif, but now the default case includes case 'a'!
+elif status in 'xyz':
+    # Do yet another thing
+else:
+    # Do the default
+
+
+
+
 
 #	BLUE : an incident (watch)
-for i in range(0, 5):
-   set_pixel(LED_number + 1, 0, 255, 0, 0.1)
+for i in range(0, 2):
+   set_pixel(LED_number + 1, 0, 255, 0, 0.2)
    show()
    time.sleep(0.40) # 1 = 1 second
-   set_pixel(LED_number + 1, 50, 205, 50, 0.2)
-#   set_pixel(LED_number + 1, 125, 255, 0, 0.2)
-#   set_pixel(LED_number + 1, 0, 0, 255, 0.2)
+   set_pixel(LED_number + 1, 0, 0, 255, 0.2)
    show()
    time.sleep(0.10) # 1 = 1 second
 
 #	YELLOW : additional incidents WARNING (alert)
-for i in range(0, 5):
+for i in range(0, 2):
    set_pixel(LED_number + 2, 0, 255, 0, 0.1)
    show()
    time.sleep(0.20) # 1 = 1 second
@@ -57,7 +70,7 @@ for i in range(0,10):
    set_pixel(LED_number + 4, 0, 0, 0, 0)
    show()
    time.sleep(0.02) # 1 = 1 second
-   set_pixel(LED_number + 4, 255, 0, 0, 0.5)
+   set_pixel(LED_number + 4, 255, 0, 0, 1.0)
    show()
    time.sleep(0.1) # 1 = 1 second
 
---
+++ blinkt/display-led.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# 	display-led.py  3.08.85  2018-03-14_21:59:15_CDT  https://github.com/BradleyA/pi-display  uadmin  two-rpi3b.cptx86.com 3.07-2-g5f6290c  
+# 	   added more prices, first draft of display-led.py 
+###
+#	The final design should control an Blinkt LED bar and
+#		display information for a second
+#		So this means it will take 8 seconds to display all LEDS?
+#	Each color level function will exit with the primary color on
+#	Color brightness controlled in each color level function
+#
+#	Other containers will update a volume that this container mounts
+#		and reads (LED_number, Level)
+###
+
+from blinkt import set_clear_on_exit, set_pixel, show, clear
+import time
+import subprocess
+
+set_clear_on_exit()
+# >>> #
+CLUSTER = "cluster-1/"
+DATA_DIR = "/usr/local/data/"
+FILE_NAME = subprocess.check_output("hostname -f", shell=True)
+# >>> #
+print  FILE_NAME
+FILE_NAME = DATA_DIR + CLUSTER + FILE_NAME
+print  FILE_NAME
+
+def status1(LED_number):
+#   Normal services and operations
+#	GREEN : no known incidents
+#   LED_number argument 0-7
+    set_pixel(LED_number, 0, 255, 0, 0.2)
+    show()
+
+def status2(LED_number):
+#   Incidents causing no disruption to overall services and operations
+#	LIGHT GREEN : an incident (watch)
+#   LED_number argument 0-7
+    for i in range(0, 5):
+        set_pixel(LED_number, 0, 0, 0, 0)
+        show()
+        time.sleep(0.05) # 1 = 1 second
+        set_pixel(LED_number, 50, 205, 50, 0.2)
+        show()
+        time.sleep(0.15) # 1 = 1 second
+        
+def status3(LED_number):
+#  Active incident with minimal affect to overall services and operations
+#	YELLOW : additional incidents WARNING (alert)
+#   LED_number argument 0-7
+    for i in range(0, 5):
+        set_pixel(LED_number, 0, 255, 0, 0.8)
+        show()
+        time.sleep(0.05) # 1 = 1 second
+        set_pixel(LED_number, 0, 0, 0, 0)
+        show()
+        time.sleep(0.03) # 1 = 1 second
+        set_pixel(LED_number, 255, 255, 0, 0.2)
+        show()
+        time.sleep(0.15) # 1 = 1 second
+
+def status4(LED_number):
+#   Active emergency incidents causing significant impact to operations and possiable service disruptions
+#	ORANGE : CRITICAL ERROR
+#   LED_number argument 0-7
+    for i in range(0,10):
+        set_pixel(LED_number, 255, 255, 0, 0.8)
+        show()
+        time.sleep(0.05) # 1 = 1 second
+        set_pixel(LED_number, 0, 0, 0, 0)
+        show()
+        time.sleep(0.03) # 1 = 1 second
+        set_pixel(LED_number, 255, 35, 0, 0.1)
+        show()
+        time.sleep(0.05) # 1 = 1 second
+
+def status5(LED_number):
+#   Active emergency incidents causing multiple impaired operations amd unavoidable severe service disruptions
+#	RED : Emergency Conditions : FATAL ERROR : 
+#   LED_number argument 0-7
+    for i in range(0,10):
+        set_pixel(LED_number, 255, 35, 0, 0.8)
+        show()
+        time.sleep(0.05) # 1 = 1 second
+        set_pixel(LED_number, 0, 0, 0, 0)
+        show()
+        time.sleep(0.03) # 1 = 1 second
+        set_pixel(LED_number, 255, 0, 0, 0.2)
+        show()
+        time.sleep(0.05) # 1 = 1 second
+
+#       VIOLET : the statistic is  WARNING (alert)
+#   LED_number argument 0-7
+def status6(LED_number):
+    set_pixel(LED_number, 127, 0, 255, 0.1)
+    show()
+    time.sleep(15) # 1 = 1 second
+
+
+status1(0)
+status2(1)
+status3(2)
+status4(3)
+status5(4)
+status6(5)
+status6(6)
+status6(7)
+
+    
+#		#   read file and process information
+#		def get_info():
+#		    file = open("/usr/local/data/cluster-1/MESSAGE","r")
+#		#    print file.read()
+#		    val = file.read()
+#		    file.close()
+#		#    val = subprocess.check_output(["uptime", "-p"]).decode("utf-8")
+#		#    val = val.replace("\n","")
+#		    val = val + " ---->  "
+#		    return val
+#		
+#		
+#		while TRUE:
+#		#   loop through leds on blinkt
+#		    for LED in range(0,7):
+#		#       if file-information(LED) == 1 then status1(LED)
+#		       if 
+#		
+#		
+#		###
