cabf0fb008acb742419fb513f03f746709d4f01c
Bradley Allen <allen.bradley@ymail.com>
2018-07-29 19:08:04 -0500

    test works for --help -help help -h h -? ?

--- scrollphat/help-message.py
+++ scrollphat/help-message.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# 	help-message.py  3.62.173  2018-07-29_19:07:08_CDT  https://github.com/BradleyA/pi-display  uadmin  three-rpi3b.cptx86.com 3.61  
+# 	   test works for --help -help help -h h -? ? 
 # 	help-message.py  3.61.172  2018-07-29_17:35:05_CDT  https://github.com/BradleyA/pi-display  uadmin  three-rpi3b.cptx86.com 3.60  
 # 	   cleanup file after completing #18 to continue with #19 
 # 	help-message.py  3.60.171  2018-07-29_17:25:31_CDT  https://github.com/BradleyA/pi-display  uadmin  three-rpi3b.cptx86.com 3.59  
@@ -28,19 +30,20 @@ class color:
    BOLD = '\033[1m'
    END = '\033[0m'
 
-#	echo -e "${NORMAL}${0} ${LINENO} [${BOLD}ERROR${NORMAL}]:       ${USER} does NOT have write permission\n\tin local Git repository directory `pwd`"      1>&2
+#	echo -e "${NORMAL}${0} ${LINENO} [${BOLD}ERROR${NORMAL}]: ${USER} does NOT have write permission\n\tin local Git repository directory `pwd`"      1>&2
 print color.END,__file__,get_line_no(),color.BOLD,"[ERROR]",color.END,"USER don't do that!\n"
 
 print "command with path =", __file__
 print "module =", __name__
 
-import os
+from os import getcwd
 
-print "pwd =", os.getcwd()
+print "pwd =", getcwd()
 
+###
 #	review github incident #15
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+print "\nNumber of arguments:", len(sys.argv), "arguments."
+print "Argument List:", str(sys.argv)
 
 ###	if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] || [ "$1" == "?" ] ; then
 ###	        display_help
@@ -50,6 +53,12 @@ print 'Argument List:', str(sys.argv)
 ###	        head -2 ${0} | awk {'print$2"\t"$3'}
 ###	        exit 0
 ###	fi
+print "first argument =", sys.argv[1]
+
+if sys.argv[1] == '--help' or sys.argv[1] == '-help' or sys.argv[1] == 'help' or sys.argv[1] == '-h' or sys.argv[1] == 'h' or sys.argv[1] == '-?' or sys.argv[1] == '?' :
+   display_help()
+   print 'test works -h  does not work --help -help help h -? ? '
+   sys.exit()
 
 import sys, getopt
 
