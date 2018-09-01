92d8992798daa9c69803d287e23c634959faa37f
Bradley Allen <allen.bradley@ymail.com>
2018-08-31 21:54:14 -0500

    begin merging standard py format

--- scrollphathd/plasma-1.py
+++ scrollphathd/plasma-1.py
@@ -1,11 +1,52 @@
 #!/usr/bin/env python
+# 	plasma-1.py  3.86.200  2018-08-31_21:53:47_CDT  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.85  
+# 	   begin merging standard py format 
 # 	plasma.py  3.78.192  2018-08-21_22:24:06_CDT  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.77  
 # 	   start design and deveopment of scrollphathd/ #22 
-
+###
 import time
 import math
-
 import scrollphathd
+###
+class color:
+   BOLD = '\033[1m'
+   END = '\033[0m'
+###
+def display_help():
+   LANGUAGE = os.getenv("LANG")
+   print "\n", __file__, "- <one line description>"
+   print "\nUSAGE\n  ", __file__, "[xxx | yyy | zzz]"
+   print "  ", __file__, "[--help | -help | help | -h | h | -? | ?] [--version | -v]"
+   print "\nDESCRIPTION\n<<your help output goes here>>"
+   print "\nOPTIONS\n   <<your options go here>>"
+   print "\nDOCUMENTATION\n   <<URL to GITHUB README>>"
+   print "\nEXAMPLES\n   <<your code examples go here>>"
+   print "      <<line two of first example>>"
+#       After displaying help in english check for other languages
+   if LANGUAGE != "en_US.UTF-8" :
+      print color.END,__file__,get_line_no(),color.BOLD,"[WARNING]",color.END,"Your language,", LANGUAGE, "is not supported, Would you like to help?"
+   return
+#
+from inspect import currentframe
+def get_line_no():
+    cf = currentframe()
+    return cf.f_back.f_lineno
+#
+no_arguments =  int(len(sys.argv))
+if no_arguments == 2 :
+   if sys.argv[1] == '--help' or sys.argv[1] == '-help' or sys.argv[1] == 'help' or sys.argv[1] == '-h' or sys.argv[1] == 'h' or sys.argv[1] == '-?' or sys.argv[1] == '?' :
+      display_help()
+      sys.exit()
+   if sys.argv[1] == '--version' or sys.argv[1] == '-version' or sys.argv[1] == 'version' or sys.argv[1] == '-v' :
+      with open(__file__) as f:
+         f.readline()
+         line2 = f.readline()
+         line2 = line2.split()
+         print line2[1], line2[2]
+      sys.exit()
+###
+
+
 
 i = 0
 
