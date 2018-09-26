01fbe8e77373ed3e9dc5895bdf05030330a5ae6a
Bradley Allen <allen.bradley@ymail.com>
2018-09-26 14:37:38 -0500

    template changes, cleanup

--- blinkt/display-led.py
+++ blinkt/display-led.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# 	display-led.py  3.159.301  2018-09-25_10:31:06_CDT  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.158  
-# 	   minor changes to display_help 
+# 	display-led.py  3.161.303  2018-09-26_14:37:10_CDT  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.160  
+# 	   template changes, cleanup 
 ###
 #	The final design should control an Blinkt LED bar on Raspberry pi and
 #		display information for a second
@@ -85,8 +85,9 @@ if no_arguments == 2 :
       sys.exit()
 
 #  DEBUG example
-import platform
-if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Name of command >{}<  Version of python >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), __file__, platform.python_version()))
+from platform import python_version
+#
+if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Name of command >{}<  Version of python >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), __file__, python_version()))
 
 #  if argument; use argument -> do not use environment variables or default for CLUSTER
 if no_arguments >= 2 :
@@ -114,9 +115,10 @@ else :
    DATA_DIR = "/usr/local/data/"
    if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  Environment variable DATA_DIR NOT set, using default >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), DATA_DIR))
 
-#  Return a fully qualified domain name
-import socket
-LOCALHOST = socket.getfqdn()
+#  Example set fully qualified domain name
+from socket import getfqdn
+#
+LOCALHOST = getfqdn()
 
 #
 FILE_NAME = DATA_DIR + "/" + CLUSTER + "/" + LOCALHOST
@@ -126,182 +128,157 @@ if DEBUG == 1 : print (">{} DEBUG{} {}  FILE_NAME >{}<".format(color.BOLD, color
 from blinkt import set_clear_on_exit, set_pixel, show, clear
 #
 set_clear_on_exit()
+time.sleep(1)	# 1 = 1 second
 
-def status1(LED_number):
 #   Normal services and operations
 #	GREEN : no known incidents
 #   LED_number argument 0-7
-    set_pixel(LED_number, 0, 255, 0, 0.2)
-    show()
-    return();
+def status1(LED_number):
+   set_pixel(LED_number, 0, 255, 0, 0.2)
+   show()
+   return();
 
-def status2(LED_number):
 #   Incidents causing no disruption to overall services and operations
 #	LIGHT GREEN : an incident (watch)
 #   LED_number argument 0-7
-    for i in range(0, 5):
-        set_pixel(LED_number, 0, 0, 0, 0)
-        show()
-        time.sleep(0.05) # 1 = 1 second
-        set_pixel(LED_number, 50, 205, 50, 0.1)
-        show()
-        time.sleep(0.15) # 1 = 1 second
-    return();
+def status2(LED_number):
+   for i in range(0, 5):
+      set_pixel(LED_number, 0, 0, 0, 0)
+      show()
+      time.sleep(0.05) # 1 = 1 second
+      set_pixel(LED_number, 50, 205, 50, 0.1)
+      show()
+      time.sleep(0.15) # 1 = 1 second
+   return();
 
-def status3(LED_number):
 #  Active incident with minimal affect to overall services and operations
 #	YELLOW : additional incidents WARNING (alert)
 #   LED_number argument 0-7
-    for i in range(0, 5):
-        set_pixel(LED_number, 0, 255, 0, 0.8)
-        show()
-        time.sleep(0.05) # 1 = 1 second
-        set_pixel(LED_number, 0, 0, 0, 0)
-        show()
-        time.sleep(0.03) # 1 = 1 second
-        set_pixel(LED_number, 255, 255, 0, 0.2)
-        show()
-        time.sleep(0.15) # 1 = 1 second
-    return();
+def status3(LED_number):
+   for i in range(0, 5):
+      set_pixel(LED_number, 0, 255, 0, 0.8)
+      show()
+      time.sleep(0.05) # 1 = 1 second
+      set_pixel(LED_number, 0, 0, 0, 0)
+      show()
+      time.sleep(0.03) # 1 = 1 second
+      set_pixel(LED_number, 255, 255, 0, 0.2)
+      show()
+      time.sleep(0.15) # 1 = 1 second
+   return();
 
-def status4(LED_number):
-#   Active emergency incidents causing significant impact to operations and possiable service disruptions
+#  Active emergency incidents causing significant impact to operations and possiable service disruptions
 #	ORANGE : CRITICAL ERROR
 #   LED_number argument 0-7
-    for i in range(0,10):
-        set_pixel(LED_number, 255, 255, 0, 0.8)
-        show()
-        time.sleep(0.05) # 1 = 1 second
-        set_pixel(LED_number, 0, 0, 0, 0)
-        show()
-        time.sleep(0.03) # 1 = 1 second
-        set_pixel(LED_number, 255, 35, 0, 0.1)
-        show()
-        time.sleep(0.05) # 1 = 1 second
-    return();
+def status4(LED_number):
+   for i in range(0,10):
+      set_pixel(LED_number, 255, 255, 0, 0.8)
+      show()
+      time.sleep(0.05) # 1 = 1 second
+      set_pixel(LED_number, 0, 0, 0, 0)
+      show()
+      time.sleep(0.03) # 1 = 1 second
+      set_pixel(LED_number, 255, 35, 0, 0.1)
+      show()
+      time.sleep(0.05) # 1 = 1 second
+   return();
 
-def status5(LED_number):
 #   Active emergency incidents causing multiple impaired operations amd unavoidable severe service disruptions
 #	RED : Emergency Conditions : FATAL ERROR : 
 #   LED_number argument 0-7
-    for i in range(0,10):
-        set_pixel(LED_number, 255, 35, 0, 0.8)
-        show()
-        time.sleep(0.05) # 1 = 1 second
-        set_pixel(LED_number, 0, 0, 0, 0)
-        show()
-        time.sleep(0.03) # 1 = 1 second
-        set_pixel(LED_number, 255, 0, 0, 0.2)
-        show()
-        time.sleep(0.05) # 1 = 1 second
-    return();
+def status5(LED_number):
+   for i in range(0,10):
+      set_pixel(LED_number, 255, 35, 0, 0.8)
+      show()
+      time.sleep(0.05) # 1 = 1 second
+      set_pixel(LED_number, 0, 0, 0, 0)
+      show()
+      time.sleep(0.03) # 1 = 1 second
+      set_pixel(LED_number, 255, 0, 0, 0.2)
+      show()
+      time.sleep(0.05) # 1 = 1 second
+   return();
 
-def status6(LED_number):
 #
 #       VIOLET : the statistic is  WARNING (alert)
 #   LED_number argument 0-7
-    set_pixel(LED_number, 127, 0, 255, 0.1)
-    show()
-    time.sleep(2) # 1 = 1 second
-    return();
+def status6(LED_number):
+   set_pixel(LED_number, 127, 0, 255, 0.1)
+   show()
+   time.sleep(2) # 1 = 1 second
+   return();
 
-def process(line):
 #   process information
-#    print("in process information function")
-    if 'celsius:' in line.lower():
-        #   print(line[line.find(':')+2:])
-        VALUE = float(line[line.find(':')+2:])
-        LED_number = 7
-        if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  celsius: VALUE >{}< LED_number >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), VALUE, LED_number))
-        if   VALUE < 48.5 : # < 48.5 C 119.3  1
-            status1(LED_number)
-        elif VALUE < 59   : # < 59 C   138.2  2
-            status2(LED_number)
-        elif VALUE < 69.5 : # < 69.5 C 157.1  3
-            status3(LED_number)
-        elif VALUE < 80   : # < 80 C   176    4
-            status4(LED_number)
-        elif VALUE >= 80  :   # > 80 C 176    5
-            status5(LED_number) 
-    if 'cpu_usage:' in line.lower():
-        #   print(line[line.find(':')+2:])
-        VALUE = int(line[line.find(':')+2:])
-        LED_number = 6
-        if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  cpu_usage: VALUE >{}< LED_number >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), VALUE, LED_number))
-        if   VALUE < 70 : # < 70 %
-            status1(LED_number)
-        elif VALUE < 80 : # < 80 %
-            status2(LED_number)
-        elif VALUE < 85 : # < 85 %
-            status3(LED_number)
-        elif VALUE < 90 : # < 90 %
-            status4(LED_number)
-        elif VALUE >= 90 : # >= 95 %
-            status5(LED_number) 
-    if 'memory_usage:' in line.lower():
-        #   print(line.split(' ')[2])
-        VALUE = int(line.split(' ')[2])
-        LED_number = 5
-        if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  memory_usage: VALUE >{}< LED_number >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), VALUE, LED_number))
-        if   VALUE < 70 : # < 70 %
-            status1(LED_number)
-        elif VALUE < 80 : # < 80 %
-            status2(LED_number)
-        elif VALUE < 85 : # < 85 %
-            status3(LED_number)
-        elif VALUE < 90 : # < 90 %
-            status4(LED_number)
-        elif VALUE >= 90 : # >= 95 %
-            status5(LED_number) 
-    if 'disk_usage:' in line.lower():
-        #   print(line.split(' ')[2])
-        VALUE = int(line.split(' ')[2])
-        LED_number = 4
-        if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  disk_usage: VALUE >{}< LED_number >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), VALUE, LED_number))
-        if   VALUE < 70 : # < 70 %
-            status1(LED_number)
-        elif VALUE < 80 : # < 80 %
-            status2(LED_number)
-        elif VALUE < 85 : # < 85 %
-            status3(LED_number)
-        elif VALUE < 90 : # < 90 %
-            status4(LED_number)
-        elif VALUE >= 90 : # >= 95 %
-            status5(LED_number) 
-    return();
+def process(line):
+   if 'celsius:' in line.lower():
+      #   print(line[line.find(':')+2:])
+      VALUE = float(line[line.find(':')+2:])
+      LED_number = 7
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  celsius: VALUE >{}< LED_number >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), VALUE, LED_number))
+      if   VALUE < 48.5 : # < 48.5 C 119.3  1
+         status1(LED_number)
+      elif VALUE < 59   : # < 59 C   138.2  2
+         status2(LED_number)
+      elif VALUE < 69.5 : # < 69.5 C 157.1  3
+         status3(LED_number)
+      elif VALUE < 80   : # < 80 C   176    4
+         status4(LED_number)
+      elif VALUE >= 80  :   # > 80 C 176    5
+         status5(LED_number) 
+   if 'cpu_usage:' in line.lower():
+      #   print(line[line.find(':')+2:])
+      VALUE = int(line[line.find(':')+2:])
+      LED_number = 6
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  cpu_usage: VALUE >{}< LED_number >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), VALUE, LED_number))
+      if   VALUE < 70 : # < 70 %
+         status1(LED_number)
+      elif VALUE < 80 : # < 80 %
+         status2(LED_number)
+      elif VALUE < 85 : # < 85 %
+         status3(LED_number)
+      elif VALUE < 90 : # < 90 %
+         status4(LED_number)
+      elif VALUE >= 90 : # >= 95 %
+         status5(LED_number) 
+   if 'memory_usage:' in line.lower():
+      #   print(line.split(' ')[2])
+      VALUE = int(line.split(' ')[2])
+      LED_number = 5
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  memory_usage: VALUE >{}< LED_number >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), VALUE, LED_number))
+      if   VALUE < 70 : # < 70 %
+         status1(LED_number)
+      elif VALUE < 80 : # < 80 %
+         status2(LED_number)
+      elif VALUE < 85 : # < 85 %
+         status3(LED_number)
+      elif VALUE < 90 : # < 90 %
+         status4(LED_number)
+      elif VALUE >= 90 : # >= 95 %
+         status5(LED_number) 
+   if 'disk_usage:' in line.lower():
+      #   print(line.split(' ')[2])
+      VALUE = int(line.split(' ')[2])
+      LED_number = 4
+      if DEBUG == 1 : print ("> {}DEBUG{} {}  {}  disk_usage: VALUE >{}< LED_number >{}<".format(color.BOLD, color.END, get_line_no(), get_date_stamp(), VALUE, LED_number))
+      if   VALUE < 70 : # < 70 %
+         status1(LED_number)
+      elif VALUE < 80 : # < 80 %
+         status2(LED_number)
+      elif VALUE < 85 : # < 85 %
+         status3(LED_number)
+      elif VALUE < 90 : # < 90 %
+         status4(LED_number)
+      elif VALUE >= 90 : # >= 95 %
+         status5(LED_number) 
+   return();
 
 #####
 #   read file and process information
-# >>>  need to replace path and file name with variables
-#	>> how to find hostname and set variable
-#	with open('/usr/local/data/us-tx-cluster-1/two-rpi3b.cptx86.com') as f:
 with open(FILE_NAME) as f:
    print ("{}{} {} {}[INFO]{}  {} {} ".format(color.END,__file__,get_line_no(),color.BOLD,color.END,time.strftime("%Y-%m-%d-%H-%M-%S-%Z"),FILE_NAME))
    for line in f:
       process(line)
-
 time.sleep(7) # 1 = 1 second
-
-# >>>  need to replace path and file name with variables
-#    file = open(FILE_NAME,"r")
-#    print file.read()
-#    file_data = fp.readlines()
-#    file.close()
-
-#		while TRUE:
-#		#   loop through leds on blinkt
-#		    for LED in range(0,7):
-#		#       if file-information(LED) == 1 then status1(LED)
-#		       if 
-###	Led test functions
-#	status1(0)
-#	status2(1)
-#	status3(2)
-#	status4(3)
-#	status5(4)
-#	status6(5)
-#   status6(6)
-#   status6(7)
 ###
-print ("\n{}{} {} {}[INFO]{}  {}  Done.\n".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp()))
+print ("{}{} {} {}[INFO]{}  {}  Done.".format(color.END, __file__, get_line_no(), color.BOLD, color.END, get_date_stamp()))
 ###
