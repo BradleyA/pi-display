03eb100a183242df54ac4f8e28ded68177d59527
Bradley Allen <allen.bradley@ymail.com>
2018-09-29 21:37:13 -0500

    include larson-1.py

---
+++ blinkt/larson-1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# 	larson-1.py  3.174.316  2018-09-29_21:36:57_CDT  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.173  
+# 	   include larson-1.py 
+
+import math
+import time
+
+from blinkt import set_clear_on_exit, set_pixel, show, set_brightness
+
+
+set_clear_on_exit()
+
+reds = [0, 0, 0, 0, 0, 16, 64, 255, 64, 16, 0, 0, 0, 0, 0]
+
+start_time = time.time()
+
+for count in range(93):
+    delta = (time.time() - start_time) * 16
+
+    # Sine wave, spends a little longer at min/max
+    # offset = int(round(((math.sin(delta) + 1) / 2) * 7))
+
+    # Triangle wave, a snappy ping-pong effect
+    offset = int(abs((delta % 16) - 8))
+
+    for i in range(8):
+        set_pixel(i , reds[offset + i], 0, 0)
+    show()
+
+    time.sleep(0.1)
