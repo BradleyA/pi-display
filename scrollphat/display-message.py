44b99b76f5abdff8d4d4e884d1ef3dac6552ff71
Bradley Allen <allen.bradley@ymail.com>
2018-12-30 19:39:43 -0600

    display-message.py Change log format and order - lint pylint pylint3 #65

--- scrollphat/display-message.py
+++ scrollphat/display-message.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# 	scrollphat/display-message.py  3.257.401  2018-12-30T19:39:43.456779-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.256-1-g52845a9  
+# 	   display-message.py Change log format and order - lint pylint pylint3 #65 
 # 	scrollphat/display-message.py  3.256.400  2018-12-30T19:09:13.189707-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.256  
 # 	   add commmet #   production standard 3 
 # 	scrollphat/display-message.py  3.256.399  2018-12-30T19:05:43.966606-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.255  
@@ -67,126 +69,131 @@ def display_help():
 #   else:
     return
 
-#  Line number function
+#   Line number function
 from inspect import currentframe
-def get_line_no() :
-   cf = currentframe()
-   return cf.f_back.f_lineno
+def get_line_no():
+    cf = currentframe()
+    return cf.f_back.f_lineno
 
-#  Date and time function ISO 8601
-def get_date_stamp() :
-   ISO8601 = time.strftime("%Y-%m-%dT%H:%M:%S%z") + time.strftime(" (%Z)")
-   return ISO8601
+#   Date and time function ISO 8601
+def get_date_stamp():
+    # calculate the offset taking into account daylight saving time
+    utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
+    utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
+    ISO8601 = datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()  + time.strftime(" (%Z)")
+#   ISO8601 = time.strftime("%Y-%m-%dT%H:%M:%S%z") + time.strftime(" (%Z)") # previous solution
+    return ISO8601
 
-#  Fully qualified domain name
+#   Fully qualified domain name
 from socket import getfqdn
-#  FQDN hostname
+#   FQDN hostname
 LOCALHOST = getfqdn()
 
-#  Version  
-with open(__file__) as f :
-   f.readline()
-   line2 = f.readline()
-   line2 = line2.split()
-   SCRIPT_NAME = line2[1]
-   SCRIPT_VERSION = line2[2]
-   f.close()
+#   Version
+with open(__file__) as f:
+    f.readline()
+    line2 = f.readline()
+    line2 = line2.split()
+    SCRIPT_NAME = line2[1]
+    SCRIPT_VERSION = line2[2]
+    f.close()
 
-#  Set user variables
-if "LOGNAME" in os.environ : LOGNAME = os.getenv("LOGNAME") # Added three lines because USER is not defined in crobtab jobs
-if "USER" in os.environ : USER = os.getenv("USER")
-else : USER = LOGNAME
+#   Set user variables
+if "LOGNAME" in os.environ: LOGNAME = os.getenv("LOGNAME") # Added three lines because USER is not defined in crobtab jobs
+if "USER" in os.environ: USER = os.getenv("USER")
+else: USER = LOGNAME
 #
 UID = os.getuid()
 GID = os.getgid()
-if DEBUG == 1 : print ("{}{} {} {} {} {}[INFO]{}  {}  {}  {} {}  Set user variables".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID))
+if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Setting USER to support crobtab...".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END))
 
-#  Default help and version arguments
-no_arguments =  int(len(sys.argv))
-if no_arguments == 2 :
-#  Default help output  
-   if sys.argv[1] == '--help' or sys.argv[1] == '-help' or sys.argv[1] == 'help' or sys.argv[1] == '-h' or sys.argv[1] == 'h' or sys.argv[1] == '-?' :
-      display_help()
-      sys.exit()
-#  Default version output  
-   if sys.argv[1] == '--version' or sys.argv[1] == '-version' or sys.argv[1] == 'version' or sys.argv[1] == '-v' :
-      print ("{} {}".format(SCRIPT_NAME, SCRIPT_VERSION))
-      sys.exit()
+#   Default help and version arguments
+no_arguments = int(len(sys.argv))
+if no_arguments == 2:
+#   Default help output
+    if sys.argv[1] == '--help' or sys.argv[1] == '-help' or sys.argv[1] == 'help' or sys.argv[1] == '-h' or sys.argv[1] == 'h' or sys.argv[1] == '-?':
+        display_help()
+        sys.exit()
+#   Default version output
+    if sys.argv[1] == '--version' or sys.argv[1] == '-version' or sys.argv[1] == 'version' or sys.argv[1] == '-v':
+        print("{} {}".format(SCRIPT_NAME, SCRIPT_VERSION))
+        sys.exit()
 
-#  Begin script INFO
-print ("{}{} {} {} {} {}[INFO]{}  {}  {}  {} {}  Begin".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID))
+#   Begin script INFO
+print("{}{} {} {}[{}] {} {} {} {}:{} {}[INFO]{}  Started...".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END))
 
-#  DEBUG example
+#   DEBUG example
 from platform import python_version
 #
-if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  Version of python >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, python_version()))
+if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Version of python >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, python_version()))
 
-#  import scrollphat and check if scrollphat installed 
-try :
-   import scrollphat 
-except ImportError :
-   if sys.version_info[0] < 3 :
-      sys.exit("{}{} {} {} {} {}[ERROR]{}  {}  {}  {} {}  Library scrollphat required. To install:\tsudo apt-get install python-scrollphat".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID))
-   elif sys.version_info[0] == 3 :
-      sys.exit("{}{} {} {} {} {}[ERROR]{}  {}  {}  {} {}  Library scrollphat required. To install:\tsudo apt-get install python3-scrollphat".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID))
-except IOError :
-      sys.exit("{}{} {} {} {} {}[ERROR]{}  {}  {}  {} {}  No such file or directory.  Is scrollphat installed on raspberry pi?".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID))
+###
+#   import scrollphat and check if scrollphat installed 
+try:
+    import scrollphat 
+except ImportError:
+    if sys.version_info[0] < 3:
+        sys.exit("{}{} {} {}[{}] {} {} {} {}:{} {}[ERROR]{}  Library scrollphat required. To install:\tsudo apt-get install python-scrollphat".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END))
+    elif sys.version_info[0] == 3 :
+        sys.exit("{}{} {} {}[{}] {} {} {} {}:{} {}[ERROR]{}  Library scrollphat required. To install:\tsudo apt-get install python-scrollphat".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END))
+except IOError:
+    sys.exit("{}{} {} {}[{}] {} {} {} {}:{} {}[ERROR]{}  No such file or directory.  Is scrollphat installed on raspberry pi?".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END))
 
-#  if argument; use argument -> do not use default or environment variables for MESSAGE
-#  # NOTE: MESSAGE is absolute path and filename else use environment variables or default to build absolute path and filename
-if no_arguments == 2 :
-#  Set non-default MESSAGE file including path
-   MESSAGE = sys.argv[1]
-   if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  Using MESSAGE file >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, MESSAGE))
-else :
-#  if no argument; -> use default if environment variables not defined
-   #  Check DATA_DIR; set using os environment variable
-   if "DATA_DIR" in os.environ :
-      DATA_DIR = os.getenv("DATA_DIR")
-      if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  Using environment variable DATA_DIR >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, DATA_DIR))
-   else :
-   #  Set DATA_DIR with default
-      DATA_DIR = "/usr/local/data/"
-      if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  Environment variable DATA_DIR NOT set, using default >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, DATA_DIR))
-   if "CLUSTER" in os.environ :
-   #  Check CLUSTER; set using os environment variable
-      CLUSTER = os.getenv("CLUSTER")
-      if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  Using environment variable CLUSTER >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, CLUSTER))
-   else :
-   #  Set CLUSTER with default
-      CLUSTER = "us-tx-cluster-1/"
-      if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  Environment variable CLUSTER NOT set, using default >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, CLUSTER))
-   if "MESSAGE_FILE" in os.environ :
-   #  Check MESSAGE_FILE; set using os environment variable
-      MESSAGE_FILE = os.getenv("MESSAGE_FILE")
-      if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  Using environment variable MESSAGE_FILE >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, MESSAGE_FILE))
-   else :
-   #  Set MESSAGE_FILE with default
-      MESSAGE_FILE = "MESSAGE"
-      if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  Environment variable MESSAGE_FILE NOT set, using default >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, MESSAGE_FILE))
-   #  Set MESSAGE with absolute path
-   MESSAGE = DATA_DIR + "/" + CLUSTER + "/" + MESSAGE_FILE
-if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  Using MESSAGE file >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, MESSAGE))
+#   if argument; use argument -> do not use default or environment variables for MESSAGE
+#   # NOTE: MESSAGE is absolute path and filename else use environment variables or default to build absolute path and filename
+if no_arguments == 2:
+#   Set non-default MESSAGE file including path
+    MESSAGE = sys.argv[1]
+    if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Using MESSAGE file >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, MESSAGE))
+else:
+#   if no argument; -> use default if environment variables not defined
+    #  Check DATA_DIR; set using os environment variable
+    if "DATA_DIR" in os.environ:
+        DATA_DIR = os.getenv("DATA_DIR")
+        if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Using environment variable DATA_DIR >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, DATA_DIR))
+    else:
+    #   Set DATA_DIR with default
+        DATA_DIR = "/usr/local/data/"
+        if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Environment variable DATA_DIR NOT set, using default >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, DATA_DIR))
+    if "CLUSTER" in os.environ:
+    #   Check CLUSTER; set using os environment variable
+        CLUSTER = os.getenv("CLUSTER")
+        if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Using environment variable CLUSTER >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, CLUSTER))
+    else:
+    #   Set CLUSTER with default
+        CLUSTER = "us-tx-cluster-1/"
+        if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Environment variable CLUSTER NOT set, using default >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, CLUSTER))
+    if "MESSAGE_FILE" in os.environ:
+    #   Check MESSAGE_FILE; set using os environment variable
+        MESSAGE_FILE = os.getenv("MESSAGE_FILE")
+        if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Using environment variable MESSAGE_FILE >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, MESSAGE_FILE))
+    else:
+    #   Set MESSAGE_FILE with default
+        MESSAGE_FILE = "MESSAGE"
+        if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Environment variable MESSAGE_FILE NOT set, using default >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, MESSAGE_FILE))
+    #   Set MESSAGE with absolute path
+    MESSAGE = DATA_DIR + "/" + CLUSTER + "/" + MESSAGE_FILE
+if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Using MESSAGE file >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, MESSAGE))
 ###
 
-#  Read TEMP_FILE contents and return contents
-def get_msg(TEMP_FILE) :
-   print ("{}{} {} {} {} {}[INFO]{}  {}  {}  {} {}  Reading MESSAGE file >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, TEMP_FILE))
-   file = open(TEMP_FILE,"r")
-   CONTENT = file.read()
-   file.close()
-   CONTENT = CONTENT.rstrip('\n')
-   if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  CONTENT >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, CONTENT))
-   return CONTENT
+#   Read TEMP_FILE contents and return contents
+def get_msg(TEMP_FILE):
+    print("{}{} {} {}[{}] {} {} {} {}:{} {}[INFO]{}  Reading MESSAGE file >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, TEMP_FILE))
+    file = open(TEMP_FILE,"r")
+    CONTENT = file.read()
+    file.close()
+    CONTENT = CONTENT.rstrip('\n')
+    if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  CONTENT >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, CONTENT))
+    return CONTENT
 
-#  timeout
+#   timeout
 def get_timeout() :
-   return ticks_per_second * refresh_interval
+    return ticks_per_second * refresh_interval
 
-#  Set brightness
+#   Set brightness
 scrollphat.set_brightness(4)
-# Every refresh_interval seconds we'll refresh
-# Only has to change every 60 seconds.
+#   Every refresh_interval seconds we'll refresh
+#   Only has to change every 60 seconds.
 pause = 0.12
 ticks_per_second = 1/pause
 refresh_interval = 60
@@ -194,26 +201,28 @@ timeout = get_timeout()
 count = 0
 scrollphat.clear_buffer()
 msg = get_msg(MESSAGE)
-if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  MESSAGE >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, msg))
+if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  MESSAGE >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, msg))
 scrollphat.set_rotate(True)
 scrollphat.write_string(msg)
-#
-while True :
-   try :
-      scrollphat.scroll()
-      time.sleep(pause)
-      if (count > timeout) :
-         scrollphat.clear_buffer()
-         msg = get_msg(MESSAGE)
-         if DEBUG == 1 : print ("{}{} {} {} {} {}[DEBUG]{}  {}  {}  {} {}  MESSAGE >{}<".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID, msg))
-         scrollphat.write_string(msg)
-         timeout = get_timeout()
-         count = 0
-      else :
-         count = count + 1
-   except KeyboardInterrupt :
-      scrollphat.clear_buffer()
-      sys.exit(-1)
 
-print ("{}{} {} {} {} {}[INFO]{}  {}  {}  {} {}  Done.".format(color.END, get_date_stamp(), __file__, SCRIPT_VERSION, get_line_no(), color.BOLD, color.END, LOCALHOST, USER, UID, GID))
+#
+while True:
+    try:
+        scrollphat.scroll()
+        time.sleep(pause)
+        if (count > timeout):
+            scrollphat.clear_buffer()
+            msg = get_msg(MESSAGE)
+            if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  MESSAGE >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, msg))
+            scrollphat.write_string(msg)
+            timeout = get_timeout()
+            count = 0
+        else:
+            count = count + 1
+    except KeyboardInterrupt:
+        scrollphat.clear_buffer()
+        sys.exit(-1)
+
+#   Done
+print("{}{} {} {}[{}] {} {} {} {}:{} {}[INFO]{}  Operation finished.".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END))
 ###
