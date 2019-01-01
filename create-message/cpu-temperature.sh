8c6f5ab25f90c7e46674ddc86e865ed269980383
Bradley Allen <allen.bradley@ymail.com>
2019-01-01 16:28:32 -0600

    renamed:    local-create-message.sh -> create-host-info.sh

--- create-message/cpu-temperature.sh
+++
@@ -1,2 +0,0 @@
-# 	create-message/cpu-temperature.sh  3.262.407  2019-01-01T15:24:24.695329-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.262  
-# 	   move cpu-temperature.sh 
