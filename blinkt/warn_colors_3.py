546878288a46b5fc58fb786b67ab1bde29b04dc5
Bradley Allen <BradleyA@users.noreply.github.com>
2019-01-01 22:02:06 -0600

    Delete warn_colors_3.py

--- blinkt/warn_colors_3.py
+++
@@ -1,2 +0,0 @@
-# 	blinkt/warn_colors_3.py  3.267.421  2019-01-01T21:59:22.050721-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.266-1-gbccfccb  
-# 	   copied to blinkt.mine 
