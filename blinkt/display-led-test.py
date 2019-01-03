f9fe8d62692f8d601f5a250eb4bde85c40053e4a
Bradley Allen <allen.bradley@ymail.com>
2019-01-03 14:49:30 -0600

    rename scrollphat.test.py to display-scrollphat-test.py

--- blinkt/display-led-test.py
+++ blinkt/display-led-test.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+# 	blinkt/display-led-test.py  3.271.430  2019-01-03T14:49:30.532723-06:00 (CST)  https://github.com/BradleyA/pi-display.git  uadmin  six-rpi3b.cptx86.com 3.270  
+# 	   rename scrollphat.test.py to display-scrollphat-test.py 
 # 	larson-1.py  3.175.317  2018-09-29_21:47:03_CDT  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.174  
 # 	   add green after red 
 # 	larson-1.py  3.174.316  2018-09-29_21:36:57_CDT  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.173  
 # 	   include larson-1.py 
+### display-led-test.py - from larson.py
 
 import math
 import time
