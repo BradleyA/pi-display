53278c5ae895cebe19f83d8ad4f590cc6375a084
Bradley Allen <BradleyA@users.noreply.github.com>
2019-01-03 15:02:13 -0600

    Delete local-create-message.sh

--- create-message/local-create-message.sh
+++
@@ -1,2 +0,0 @@
-# 	create-message/local-create-message.sh  3.263.409  2019-01-01T16:28:32.820883-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.262-2-gec7c8dc  
-# 	   renamed:    local-create-message.sh -> create-host-info.sh 
