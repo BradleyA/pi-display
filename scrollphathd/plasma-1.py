373c78287c585276cd3d879825e313249c0a8a36
Bradley Allen <BradleyA@users.noreply.github.com>
2019-01-03 14:56:47 -0600

    Delete plasma-1.py

--- scrollphathd/plasma-1.py
+++
@@ -1,2 +0,0 @@
-# 	scrollphathd/plasma-1.py  3.272.434  2019-01-03T14:56:29.897446-06:00 (CST)  https://github.com/BradleyA/pi-display.git  uadmin  six-rpi3b.cptx86.com 3.271-3-g7cd8588  
-# 	   rename plasma-1.py to display-scrollphathd-test.py 
