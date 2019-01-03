70efbf07272ecd7005128f60623d1ffc4e07594a
Bradley Allen <BradleyA@users.noreply.github.com>
2019-01-03 15:01:21 -0600

    Delete create-message.sh

--- create-message/create-message.sh
+++
@@ -1,2 +0,0 @@
-# 	create-message/create-message.sh  3.273.436  2019-01-03T15:00:31.612663-06:00 (CST)  https://github.com/BradleyA/pi-display.git  uadmin  six-rpi3b.cptx86.com 3.272-1-g373c782  
-# 	   rename create-message.sh to create-display-message.sh 
