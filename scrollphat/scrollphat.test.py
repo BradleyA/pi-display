7cd8588fc63a7c944f9794805dd010921d240344
Bradley Allen <BradleyA@users.noreply.github.com>
2019-01-03 14:52:05 -0600

    Delete scrollphat.test.py

--- scrollphat/scrollphat.test.py
+++
@@ -1,2 +0,0 @@
-# 	scrollphat/scrollphat.test.py  3.271.430  2019-01-03T14:49:30.616088-06:00 (CST)  https://github.com/BradleyA/pi-display.git  uadmin  six-rpi3b.cptx86.com 3.270  
-# 	   rename scrollphat.test.py to display-scrollphat-test.py 
