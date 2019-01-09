f48bc027485deed3fe5abbbf0dbfcfa66a05e475
Bradley Allen <allen.bradley@ymail.com>
2019-01-09 13:23:45 -0600

    DEBUG

--- scrollphat/display-message.py
+++ scrollphat/display-message.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# 	scrollphat/display-message.py  3.313.499  2019-01-09T13:23:45.241389-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.312  
+# 	   DEBUG 
 # 	scrollphat/display-message.py  3.312.498  2019-01-09T12:23:21.538299-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.311  
 # 	   DEBUG while loop 
 # 	scrollphat/display-message.py  3.277.451  2019-01-05T08:52:02.879865-06:00 (CST)  https://github.com/BradleyA/pi-display.git  uadmin  six-rpi3b.cptx86.com 3.276  
@@ -14,7 +16,7 @@ import time
 import os
 import subprocess
 #       Order of precedence: environment variable (export DEBUG=1), default code
-DEBUG = int(os.getenv("DEBUG", 0)) #  Set DEBUG,  0 = debug off, 1 = debug on, 'unset DEBUG' to unset environment variable (bash)
+DEBUG = int(os.getenv("DEBUG", 1)) #  Set DEBUG,  0 = debug off, 1 = debug on, 'unset DEBUG' to unset environment variable (bash)
 ###
 class color:
     BOLD = '\033[1m'
@@ -219,11 +221,14 @@ while True:
             if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{} -->  count >{}< timeout >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, count, timeout))
             count = 0
             if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{} -->  count >{}< timeout >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, count, timeout))
+            scrollphat.clear_buffer()
+            scrollphat.clear()
+            sys.exit(-1)
         else:
             count = count + 1
             if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{} --->  count >{}< timeout >{}<".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, count, timeout))
     except KeyboardInterrupt:
-        scrollphat.clear_buffer()
+        scrollphat.clear()
         sys.exit(-1)
 
 #   Done
