9a67518038e966dade5ec3048705c839fd08ee99
Bradley Allen <allen.bradley@ymail.com>
2019-01-23 21:51:47 -0600

    second pass to add production standards

--- blinkt/display-led-test.py
+++ blinkt/display-led-test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# 	blinkt/display-led-test.py  3.373.569  2019-01-23T21:51:47.651894-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.372-6-g8d92fb5  
+# 	   second pass to add production standards 
 # 	blinkt/display-led-test.py  3.372.562  2019-01-23T21:36:59.212435-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.371  
 # 	   first pass to add production standards 
 # 	blinkt/display-led-test.py  3.371.561  2019-01-23T21:13:33.503303-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.370  
@@ -29,9 +31,94 @@ class color:
 ###
 LANGUAGE = os.getenv("LANG")
 def display_help():
+    print("\n{} - Test leds".format(__file__))
+    print("\nUSAGE\n   {}".format(__file__))
+    print("   {} [--help | -help | help | -h | h | -?]".format(__file__))
+    print("   {} [--version | -version | -v]".format(__file__))
+    print("\nDESCRIPTION")
+#   Displaying help DESCRIPTION in English en_US.UTF-8
+    print("This script tests the leds during system boot.  The script can be run on the")
+    print("command line.  It is designed to be run fron crontab during system boot.  It")
+    print("is configured using, crontab -e")
+#       Displaying help DESCRIPTION in French
+    if (LANGUAGE == "fr_CA.UTF-8") or (LANGUAGE == "fr_FR.UTF-8") or (LANGUAGE == "fr_CH.UTF-8"):
+        print("\n--> {}".format(LANGUAGE))
+        print("<votre aide va ici>")
+        print("Souhaitez-vous traduire la section description?")
+    elif (LANGUAGE != "en_US.UTF-8"):
+        print("{}{} {} {}[{}] {} {} {} {}:{} {}[INFO]{}  {} is not supported, Would you like to help translate the description section?".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END, LANGUAGE))
+    print("\nEnvironment Variables")
+    print("If using the bash shell, enter; 'export DEBUG=1' on the command line to set")
+    print("the DEBUG environment variable to '1' (0 = debug off, 1 = debug on).  Use the")
+    print("command, 'unset DEBUG' to remove the exported information from the DEBUG")
+    print("environment variable.  You are on your own defining environment variables if")
+    print("you are using other shells.")
+    print("   DEBUG       (default '0')")
+    print("\n   <<your environment variables information goes here>>")
+    print("\nOPTIONS\n   <<your options go here>>")
+    print("\nDOCUMENTATION\n    https://github.com/BradleyA/   <<URL to online repository README>>")
+    print("\nEXAMPLES\n   <<your code examples description goes here>>")
+    print("   {} <<code example goes here>>\n".format(__file__))
+    return
 
+#   Line number function
+from inspect import currentframe
+def get_line_no():
+    cf = currentframe()
+    return cf.f_back.f_lineno
 
+#   Date and time function ISO 8601
+def get_date_stamp():
+    # calculate the offset taking into account daylight saving time
+    utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
+    utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
+    ISO8601 = datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()  + time.strftime(" (%Z)")
+#   ISO8601 = time.strftime("%Y-%m-%dT%H:%M:%S%z") + time.strftime(" (%Z)") # previous solution
+    return ISO8601
+
+#   Fully qualified domain name
+from socket import getfqdn
+#   FQDN hostname
+LOCALHOST = getfqdn()
+
+#   Version
+with open(__file__) as f:
+    f.readline()
+    line2 = f.readline()
+    line2 = line2.split()
+    SCRIPT_NAME = line2[1]
+    SCRIPT_VERSION = line2[2]
+    f.close()
+
+#   Set user variables
+if "LOGNAME" in os.environ: LOGNAME = os.getenv("LOGNAME") # Added three lines because USER is not defined in crobtab jobs
+if "USER" in os.environ: USER = os.getenv("USER")
+else: USER = LOGNAME
 #
+UID = os.getuid()
+GID = os.getgid()
+if DEBUG == 1: print("{}{} {} {}[{}] {} {} {} {}:{} {}[DEBUG]{}  Setting USER to support crobtab...".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END))
+
+#   Default help and version arguments
+no_arguments = int(len(sys.argv))
+if no_arguments == 2:
+#   Default help output
+    if sys.argv[1] == '--help' or sys.argv[1] == '-help' or sys.argv[1] == 'help' or sys.argv[1] == '-h' or sys.argv[1] == 'h' or sys.argv[1] == '-?':
+        display_help()
+        sys.exit()
+#   Default version output
+    if sys.argv[1] == '--version' or sys.argv[1] == '-version' or sys.argv[1] == 'version' or sys.argv[1] == '-v':
+        print("{} {}".format(SCRIPT_NAME, SCRIPT_VERSION))
+        sys.exit()
+
+#   Begin script INFO
+print("{}{} {} {}[{}] {} {} {} {}:{} {}[INFO]{}  Started...".format(color.END, get_date_stamp(), LOCALHOST, __file__, os.getpid(), SCRIPT_VERSION, get_line_no(), USER, UID, GID, color.BOLD, color.END))
+
+
+
+
+
+###
 set_clear_on_exit()
 
 reds = [0, 0, 0, 0, 0, 16, 64, 255, 64, 16, 0, 0, 0, 0, 0]
