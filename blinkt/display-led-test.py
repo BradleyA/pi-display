7d834f095104b52ccdcfc48d1b38b8426045a3e1
Bradley Allen <allen.bradley@ymail.com>
2019-01-23 20:42:21 -0600

    added led color test during start

--- blinkt/display-led-test.py
+++ blinkt/display-led-test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# 	blinkt/display-led-test.py  3.369.559  2019-01-23T20:42:21.093603-06:00 (CST)  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.368  
+# 	   added led color test during start 
 # 	blinkt/display-led-test.py  3.271.430  2019-01-03T14:49:30.532723-06:00 (CST)  https://github.com/BradleyA/pi-display.git  uadmin  six-rpi3b.cptx86.com 3.270  
 # 	   rename scrollphat.test.py to display-scrollphat-test.py 
 # 	larson-1.py  3.175.317  2018-09-29_21:47:03_CDT  https://github.com/BradleyA/pi-display  uadmin  six-rpi3b.cptx86.com 3.174  
@@ -9,8 +11,9 @@
 
 import math
 import time
-
+import colorsys
 from blinkt import set_clear_on_exit, set_pixel, show, set_brightness
+
 #
 set_clear_on_exit()
 
@@ -47,4 +50,20 @@ for count in range(47):
     show()
 
     time.sleep(0.1)
+
+set_brightness(0.1)
+
+spacing = 360.0 / 16.0
+hue = 0
+x = 0
+j = 1
+for j in range(4000):
+    hue = int(time.time() * 100) % 360
+    for x in range(8):
+        offset = x * spacing
+        h = ((hue + offset) % 360) / 360.0
+        r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
+        set_pixel(x, r, g, b)
+    show()
+    time.sleep(0.001)
 
